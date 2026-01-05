22a410ad93ad07ea77a7b83b4852db79c2a36aec
Brandon <isBrandino@outlook.com>
2026-01-05 14:30:25 -0500

    install fix

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 alias llama="$PWD/run.sh"
+files/.venv/bin/python -m ensurepip --upgrade || files/.venv/bin/python -m pip install --upgrade pip setuptools wheel
 $PWD/files/edit.sh
 $PWD/files/edit.sh ollama
 $PWD/files/edit.sh uuid
